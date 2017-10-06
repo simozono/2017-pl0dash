5e890520f0fb81c75dd1a2b7de71e9ab70d5ba09
Koichi SHIMOZONO <simozono@cc.kagoshima-u.ac.jp>
2017-10-06 10:18:24 +0900

    さらにコード整形

--- vcpu/pl0dashvm.rb
+++ vcpu/pl0dashvm.rb
@@ -8,19 +8,15 @@ class Pl0DashVM
 
   def initialize(lines)
     init_reg_mem # レジスタとメモリ初期化
-    i = 1
-    lines.each do |line|
+    lines.each.with_index(1) do |line, i|
       @memory[i] = line.split(/\s+|\s*,\s*/)
-      i += 1
     end
   end
 
   def list_code_area
-    i = 1
-    @memory[1..(HEAP_START - 1)].each do |item|
+    @memory[1..(HEAP_START - 1)].each.with_index(1) do |item, i|
       break if item.nil?
       printf "%4d %s\n", i, [item[0], item[1..2].join(',')].join(' ').strip
-      i += 1
     end
   end
 
@@ -32,97 +28,93 @@ class Pl0DashVM
         printf "%4d %s\n", @reg[:pc],
                [item[0], item[1..2].join(',')].join(' ').strip
       end
-      status = execute_line(@memory[@reg[:pc]])
-      if show
-        show_reg
-        show_heap
-        show_stack
-      end
-      break unless status
+      exec_status = execute_line(@memory[@reg[:pc]])
+      show_vcpu_status if show
+      break unless exec_status
     end
   end
 
   def execute_line(line)
     (op, opr1, opr2) = line
     case op.upcase
-      when 'LOAD'
-        move(ref_op(opr2), opr1)
-        @reg[:pc] += 1
-      when 'STORE', 'MOVE'
-        move(ref_op(opr1), opr2)
-        @reg[:pc] += 1
-      when 'PUSH'
-        @reg[:sp] -= 1
-        move(ref_op(opr1), '#(SP)')
-        @reg[:pc] += 1
-      when 'POP'
-        move(@memory[@reg[:sp]], opr1)
-        @reg[:sp] += 1
-        @reg[:pc] += 1
-      when 'JMP'
+    when 'LOAD'
+      move(ref_op(opr2), opr1)
+      @reg[:pc] += 1
+    when 'STORE', 'MOVE'
+      move(ref_op(opr1), opr2)
+      @reg[:pc] += 1
+    when 'PUSH'
+      @reg[:sp] -= 1
+      move(ref_op(opr1), '#(SP)')
+      @reg[:pc] += 1
+    when 'POP'
+      move(@memory[@reg[:sp]], opr1)
+      @reg[:sp] += 1
+      @reg[:pc] += 1
+    when 'JMP'
+      @reg[:pc] = ref_op(opr1)
+    when 'JPC'
+      if (@reg[:c]).zero?
         @reg[:pc] = ref_op(opr1)
-      when 'JPC'
-        if (@reg[:c]).zero?
-          @reg[:pc] = ref_op(opr1)
-        else
-          @reg[:pc] += 1
-        end
-      when 'PUSHUP'
-        @reg[:sp] -= 1
-        @reg[:pc] += 1
-      when 'CALL'
-        @reg[:sp] -= 1
-        @memory[@reg[:sp]] = @reg[:pc] + 1
-        @reg[:pc] = ref_op(opr1)
-      when 'RET'
-        @reg[:pc] = @memory[@reg[:sp]]
-        @reg[:sp] += 1
-        @reg[:sp] += ref_op(opr1)
-      when 'PRINT'
-        print ref_op(opr1)
-        @reg[:pc] += 1
-      when 'PRINTLN'
-        puts
-        @reg[:pc] += 1
-      when 'END'
-        @reg[:pc] = -1
-        return false
-      # 以下演算子
-      when 'PLUS'
-        @reg[:c]  = @reg[:a] + @reg[:b]
-        @reg[:pc] += 1
-      when 'MINUS'
-        @reg[:c]  = @reg[:a] - @reg[:b]
-        @reg[:pc] += 1
-      when 'MULTI'
-        @reg[:c]  = @reg[:a] * @reg[:b]
-        @reg[:pc] += 1
-      when 'DIV'
-        @reg[:c]  = (@reg[:a] / @reg[:b]).to_i
-        @reg[:pc] += 1
-      when 'CMPODD'
-        @reg[:c]  = @reg[:a].odd? ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPEQ'
-        @reg[:c]  = @reg[:a].to_i == @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPNOTEQ'
-        @reg[:c]  = @reg[:a].to_i != @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPLT'
-        @reg[:c]  = @reg[:a].to_i < @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPGT'
-        @reg[:c]  = @reg[:a].to_i > @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPLE'
-        @reg[:c]  = @reg[:a].to_i <= @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
-      when 'CMPGE'
-        @reg[:c]  = @reg[:a].to_i >= @reg[:b].to_i ? 1 : 0
-        @reg[:pc] += 1
       else
-        abort 'ASM CODE ERROR'
+        @reg[:pc] += 1
+      end
+    when 'PUSHUP'
+      @reg[:sp] -= 1
+      @reg[:pc] += 1
+    when 'CALL'
+      @reg[:sp] -= 1
+      @memory[@reg[:sp]] = @reg[:pc] + 1
+      @reg[:pc]          = ref_op(opr1)
+    when 'RET'
+      @reg[:pc] = @memory[@reg[:sp]]
+      @reg[:sp] += 1
+      @reg[:sp] += ref_op(opr1)
+    when 'PRINT'
+      print ref_op(opr1)
+      @reg[:pc] += 1
+    when 'PRINTLN'
+      puts
+      @reg[:pc] += 1
+    when 'END'
+      @reg[:pc] = -1
+      return false
+      # 以下演算子
+    when 'PLUS'
+      @reg[:c] = @reg[:a] + @reg[:b]
+      @reg[:pc] += 1
+    when 'MINUS'
+      @reg[:c] = @reg[:a] - @reg[:b]
+      @reg[:pc] += 1
+    when 'MULTI'
+      @reg[:c] = @reg[:a] * @reg[:b]
+      @reg[:pc] += 1
+    when 'DIV'
+      @reg[:c] = (@reg[:a] / @reg[:b]).to_i
+      @reg[:pc] += 1
+    when 'CMPODD'
+      @reg[:c] = @reg[:a].odd? ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPEQ'
+      @reg[:c] = @reg[:a].to_i == @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPNOTEQ'
+      @reg[:c] = @reg[:a].to_i != @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPLT'
+      @reg[:c] = @reg[:a].to_i < @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPGT'
+      @reg[:c] = @reg[:a].to_i > @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPLE'
+      @reg[:c] = @reg[:a].to_i <= @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    when 'CMPGE'
+      @reg[:c] = @reg[:a].to_i >= @reg[:b].to_i ? 1 : 0
+      @reg[:pc] += 1
+    else
+      abort 'ASM CODE ERROR'
     end
   end
 
@@ -130,33 +122,33 @@ class Pl0DashVM
 
   def move(value, sto_op) # valueをsto_opに代入
     case sto_op.downcase
-      when /^([a-z]+)$/ # レジスタのはず
-        @reg[$1.to_sym] = value
-      when /^#\((\d+)\)$/ # #(数字)のはず
-        @memory[$1.to_i] = value
-      when /^#\(([a-z]+)\)$/ # #(レジスタ)のはず
-        @memory[@reg[$1.to_sym]] = value
-      when /^#\(fp([+|\-]\d+)\)$/ # #(FP(+|-)数字)のはず
-        @memory[@reg[:fp] + $1.to_i] = value
-      else
-        abort 'ASM OPERAND2 ERROR'
+    when /^([a-z]+)$/ # レジスタのはず
+      @reg[Regexp.last_match(1).to_sym] = value
+    when /^#\((\d+)\)$/ # #(数字)のはず
+      @memory[Regexp.last_match(1).to_i] = value
+    when /^#\(([a-z]+)\)$/ # #(レジスタ)のはず
+      @memory[@reg[Regexp.last_match(1).to_sym]] = value
+    when /^#\(fp([+|\-]\d+)\)$/ # #(FP(+|-)数字)のはず
+      @memory[@reg[:fp] + Regexp.last_match(1).to_i] = value
+    else
+      abort 'ASM OPERAND2 ERROR'
     end
   end
 
   def ref_op(opr)
     case opr.downcase
-      when /^(\d+)$/ # 数字
-        $1.to_i
-      when /^([a-z]+)$/ # レジスタのはず
-        @reg[$1.to_sym]
-      when /^#\((\d+)\)$/ # #(数字)のはず
-        @memory[$1.to_i]
-      when /^#\(([a-z]+)\)$/ # #(レジスタ)のはず
-        @memory[@reg[$1.to_sym]]
-      when /^#\(fp([+|\-]\d+)\)$/ # #(FP(+|-)数字)のはず
-        @memory[@reg[:fp] + $1.to_i]
-      else
-        abort 'ASM OPERAND1 ERROR'
+    when /^(\d+)$/ # 数字
+      Regexp.last_match(1).to_i
+    when /^([a-z]+)$/ # レジスタのはず
+      @reg[Regexp.last_match(1).to_sym]
+    when /^#\((\d+)\)$/ # #(数字)のはず
+      @memory[Regexp.last_match(1).to_i]
+    when /^#\(([a-z]+)\)$/ # #(レジスタ)のはず
+      @memory[@reg[Regexp.last_match(1).to_sym]]
+    when /^#\(fp([+|\-]\d+)\)$/ # #(FP(+|-)数字)のはず
+      @memory[@reg[:fp] + Regexp.last_match(1).to_i]
+    else
+      abort 'ASM OPERAND1 ERROR'
     end
   end
 
@@ -170,6 +162,12 @@ class Pl0DashVM
     @reg[:sp]  = MAX_MEM + 1
     @reg[:fp]  = MAX_MEM + 1
     @reg[:pc]  = 0
+  end
+
+  def show_vcpu_status
+    show_reg
+    show_heap
+    show_stack
   end
 
   def show_reg
